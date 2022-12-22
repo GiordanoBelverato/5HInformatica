8dc21ef544b470c23532cb8fdf3e0f9cf9899636
GiordanoBelverato <113595202+GiordanoBelverato@users.noreply.github.com>
2022-12-22 11:37:44 +0100

    Create Logout.php

---
+++ PHP/ES04/Logout.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Uscita - ITCS Erasmo da Rotterdam</title>
+</head>
+
+<body>
+
+<?php
+
+session_destroy();
+echo"Logout avvenuto, Arrivederci";
+
+?>
+<hr>
+	<a href="Login.php"><h2>Accedi di nuovo</h2></a>
+	<a href="index_session.php><h2>Torna alla home</h2></a>
+	
+</body>
+</html>
