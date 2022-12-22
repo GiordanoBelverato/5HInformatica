a1f1ad024149857fb85242adae25e64dc13c6811
GiordanoBelverato <113595202+GiordanoBelverato@users.noreply.github.com>
2022-12-22 11:35:02 +0100

    Update and rename Index to index_sessione.php

---
+++ PHP/ES04/index_sessione.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+<h1>Home page</h1>
+<a href="Login.php"><h2>Esegui Login</a><br/></h2>
+<a href="PagRiservata.php"><h2>Vai alla pagina riservata</a><br/></h2>
+<a href="Logout.php"><h2>Esegui il Logout</a><br/></h2>
+<hr/>
+</body>
+</html>
